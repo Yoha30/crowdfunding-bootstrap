3bb63e255d1c2e60ec341a3157c7c1f502725bdc
Yoha30 <3202114888@alexu.edu.eg>
2021-09-29 18:22:15 +0200

    bootstrap

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let grayBookmark = document.getElementById('gray-bookmark');
 let greenBookmark = document.getElementById('green-bookmark');
 let bookmarkColor = document.getElementById('bookmark-color');
 var bookmarked = document.createTextNode("ed");
+let firstSelcted = document.getElementById('selcted-section');
+let productsLeft = document.getElementById('products-left');
+let backProject = document.getElementById('back-project');
+let backgroundImage = document.getElementById('background-image');
+let aboutSection = document.getElementById('about-this');
+let logo = document.getElementById('logo');
+let blackeditionLeft = document.getElementById('blackedition-left');
+let secondSection = document.getElementById('second-section');
+let mahogany = document.getElementById('mahogany-selcted');
+let mahoganyLeft = document.getElementById('mahogany-left');
 function bookMark() {
     if (greenBookmark.classList.contains('d-none')) {
         grayBookmark.classList.add('d-none');
@@ -15,3 +25,33 @@ function bookMark() {
         bookmarkColor.removeChild(bookmarked);
     }
 }
+function checked() {
+    firstSelcted.classList.remove('d-none');
+    productsLeft.classList.add('bamboostand-left-opened');
+    productsLeft.classList.remove('bamboostand-left');
+    blackeditionLeft.classList.add('blackedition-left-opened');
+}
+function backThisProject() {
+    if (backProject.classList.contains('d-none')) {
+        backProject.classList.remove('d-none');
+        backgroundImage.classList.add('opacity-50');
+        aboutSection.classList.add('opacity-50');
+        logo.classList.add('d-none');
+    }
+}
+function closeModal() {
+    if (backgroundImage.classList.contains('opacity-50')) {
+        backProject.classList.add('d-none');
+        backgroundImage.classList.remove('opacity-50');
+        aboutSection.classList.remove('opacity-50');
+        logo.classList.remove('d-none');
+    }
+}
+function blackedition() {
+    secondSection.classList.remove('d-none');
+}
+function mahoganySpecial(){
+    mahogany.classList.remove('d-none');
+    mahoganyLeft.classList.add('mahogany-left-opened');
+}
+
